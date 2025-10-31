792517a897806219ffcb30c4934a79f2580f86a4
Maunducyrus <maunducyrus123@gmail.com>
2025-10-31 11:48:48 +0300

    Updated TaskItem.jsx to add edit mode toggling and input fields.

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -1,13 +1,50 @@
 // Displays individual tasks.
+import { useState } from "react";
 
-function TaskItem({ task, onToggle, onDelete}) {
+function TaskItem({ task, onToggle, onDelete, onEdit }) {
+    const [isEditing, SetIsEditing] = useState(false);
+    const [editedTitle, setEditedTitle] = useState(task.title);
+    const [editedDesc, setEditedDesc] = useState(task.description)
+
+    const handleSave = () => {
+        onEdit(task.id, editedTitle, editedDesc);
+        SetIsEditing(false);
+    }
     return (
         <div
-        className={`flex justify-between items-start p-3 rounded border shadow-sm ${
+        className={`flex flex-col sm:flex-row justify-between gap-3 items-start p-3 rounded border shadow-sm ${
             task.completed ? "bg-green-100" : "bg-white"
             }`}
             >
-                <div>
+                <div className="flex-1">
+                    {isEditing ? (
+                        <>
+                        <input
+                        type="text"
+                        value={editedTitle}
+                        onChange={(e) => setEditedTitle(e.target.value)}
+                        className="border p-1 w-full rounded mb-2"
+                        />
+                        <textarea
+                        value={editedDesc}
+                        onChange={(e) => setEditedDesc(e.target.value)}
+                        className="border p-1 w-full rounded"
+                        />
+                        </>
+                    ) : (
+                        <>
+                        <h3
+                        className={`font-semibold $ {
+                            task.completed ? "line-through text-gray-500" : ""
+                            }`}
+                            >
+                                {task.title}
+                            </h3>
+                            <p className="text-sm text-gray-600">{task.description}</p>
+                        </>
+                    )}
+                </div>
+                {/* <div>
                     <h3 className={`font-semibold ${
                         task.completed ? "line-through text-gray-500" : ""
                     }`}
@@ -15,18 +52,44 @@ function TaskItem({ task, onToggle, onDelete}) {
                         {task.title}
                     </h3>
                     <p className="text-sm text-gray-600">{task.description}</p>
-                </div>
+                </div> */}
                 <div className="flex gap-2">
+                    {isEditing ? (
+                        <>
+                        <button
+                        onClick={handleSave}
+                        className="text-sm bg-blue-500 text-white px-2 py-1 rounded"
+                        >
+                            Save
+                        </button>
+                        <button
+                        onClick={() => SetIsEditing(false)}
+                        className="text-sm bg-gray-400 text-white px-2 py-1 rounded"
+                        >
+                            Cancel
+                        </button>
+                        </>
+                    ) : (
+                        <>
                     <button
                     onClick={() => onToggle(task.id)}
                     className="text-sm bg-green-500 text-white px-2 py-1 rounded">
                         {task.completed ? "Undo" : "Done"}
                     </button>
                     <button
+                    onClick={() => SetIsEditing(true)}
+                    className="text-sm bg-yellow-500 text-white px-2 py-1 rounded"
+                    >
+                        Edit
+                    </button>
+                    <button
                     onClick={() => onDelete(task.id)}
                     className="text-sm bg-red-500 text-white px-2 py-2 rounded">
                         Delete
                     </button>
+                        </>
+                    )}
+
                 </div>
                 </div>
     );
